cebdb57bc6a303c27f04e512da1e467b692a84f3
Chen Huang <alan.tolearn@gmail.com>
2014-10-30 16:21:44 -0400

    User can query hotel information

--- hotel_database.rb
+++ hotel_database.rb
@@ -7,13 +7,39 @@ class HotelDatabase
     read_hotel_info(file)
   end
 
+  def query_hotel
+    hotel_name = query_user_input
+    matched_hotel = find_hotel(hotel_name)
+    list_hotel(matched_hotel)
+  end
+
+  private
+
   def read_hotel_info(file)
     CSV.foreach(file, headers: true) do |row|
       hotel_name = row["Hotel"]
       @hotels[hotel_name] = row
     end
   end
+
+  def query_user_input
+    print "Which hotel information you want to know >> "
+    gets.chomp
+  end
+
+  def find_hotel(name)
+    @hotels[name]
+  end
+
+  def list_hotel(hotel_info)
+    puts "Name: #{hotel_info["Hotel"]}"
+    puts "Location: #{hotel_info["City"]}"
+    puts "Phone Number: #{hotel_info["Phone Number"]}"
+    puts "Number of Rooms: #{hotel_info["Number of Singles"] + hotel_info["Number of Doubles"]}"
+  end
 end
 
 file = "hotels.csv"
-HotelDatabase.new(file)
+hotel_db = HotelDatabase.new(file)
+hotel_db.query_hotel
+
